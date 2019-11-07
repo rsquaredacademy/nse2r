13559d393f43114379f4572570ebaa8245a4a0e2
rsquaredin <rsquaredin@gmail.com>
2019-11-07 17:22:48 +0530

    closes #3

---
+++ R/top-gainers.R
@@ -0,0 +1,37 @@
+#' NSE top gainers
+#'
+#' Top gainers for the last trading session.
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
+nse_top_gainers <- function() {
+
+  url <- "http://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
