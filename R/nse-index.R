e7771df3779a02b6ec25179ea1bc23ee80cd781d
rsquaredin <rsquaredin@gmail.com>
2019-11-08 15:55:46 +0530

    closes #7

---
+++ R/nse-index.R
@@ -0,0 +1,36 @@
+#' Index quote
+#'
+#' NSE indices quote
+#'
+#' @export
+#'
+nse_index_quote <- function() {
+
+  url <- "http://www.nseindia.com/homepage/Indices1.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data) %>%
+      magrittr::extract(-5)
+  }
+
+}
