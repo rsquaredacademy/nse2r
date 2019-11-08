913b2ca41defe1de0b9245b298e5b41c6b0ebb28
rsquaredin <rsquaredin@gmail.com>
2019-11-08 16:10:45 +0530

    closes #14

---
+++ R/nse-stock.R
@@ -0,0 +1,35 @@
+#' Most actively traded stocks
+#'
+#' Most actively traded stocks in a month on NSE.
+#'
+#' @export
+#'
+nse_stock_most_traded <- function() {
+
+  url <- "https://www.nseindia.com/products/dynaContent/equities/equities/json/mostActiveMonthly.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
