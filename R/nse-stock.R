ee12bfd30945d862754d27e272841a51e31d67bc
rsquaredin <rsquaredin@gmail.com>
2019-11-08 16:20:36 +0530

    closes #15

--- R/nse-stock.R
+++ R/nse-stock.R
@@ -33,3 +33,39 @@ nse_stock_most_traded <- function() {
   }
 
 }
+
+#' 52 week high
+#'
+#' Stocks that have touched their 52 week highs during the day on NSE.
+#'
+#' @export
+#'
+nse_stock_year_high <- function() {
+
+  url <- "https://www.nseindia.com/products/dynaContent/equities/equities/json/online52NewHigh.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
