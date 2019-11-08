981dee2a13f7c5e47b29eae54fc444558128b068
rsquaredin <rsquaredin@gmail.com>
2019-11-08 15:34:51 +0530

    closes #13

--- R/top-losers.R
+++ R/top-losers.R
@@ -34,3 +34,38 @@ nse_top_losers <- function() {
 
 }
 
+#' NSE top F & O losers
+#'
+#' Top futures and options losers for the last trading session.
+#'
+#' @export
+#'
+nse_top_losers_fo <- function() {
+
+  url <- "https://www.nseindia.com/live_market/dynaContent/live_analysis/losers/fnoLosers1.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
