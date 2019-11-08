d0dd418f7c291c64713033c05ebcacc46ff24d2e
rsquaredin <rsquaredin@gmail.com>
2019-11-08 15:13:54 +0530

    closes #10

---
+++ R/top-losers.R
@@ -0,0 +1,35 @@
+#' NSE top losers
+#'
+#' Top losers for the last trading session.
+#'
+#' @export
+#'
+nse_top_losers <- function() {
+
+  url <- "http://www.nseindia.com/live_market/dynaContent/live_analysis/losers/niftyLosers1.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
