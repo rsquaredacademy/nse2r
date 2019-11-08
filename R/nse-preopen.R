3c796a45e7a66e25868e097a97ec3e2393ac6dbe
rsquaredin <rsquaredin@gmail.com>
2019-11-08 16:47:46 +0530

    closes #18

--- R/nse-preopen.R
+++ R/nse-preopen.R
@@ -33,3 +33,40 @@ nse_preopen_nifty <- function() {
   }
 
 }
+
+#' Pre open nifty bank
+#'
+#' Fetch data of pre open session of Nifty Bank from NSE.
+#'
+#' @export
+#'
+nse_preopen_nifty_bank <- function() {
+
+  url <- "https://www.nseindia.com/live_market/dynaContent/live_analysis/pre_open/niftybank.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
+
