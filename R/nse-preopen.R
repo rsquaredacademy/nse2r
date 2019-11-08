fa858a8bb1e9e2d6a6023d64ca68cc0b80566652
rsquaredin <rsquaredin@gmail.com>
2019-11-08 16:53:43 +0530

    closes #19

--- R/nse-preopen.R
+++ R/nse-preopen.R
@@ -70,3 +70,38 @@ nse_preopen_nifty_bank <- function() {
 
 }
 
+#' Pre open F & O
+#'
+#' Fetch data of pre open session of futures and options from NSE.
+#'
+#' @export
+#'
+nse_preopen_fo <- function() {
+
+  url <- "https://www.nseindia.com/live_market/dynaContent/live_analysis/pre_open/fo.json"
+
+  is_online <- pingr::is_online()
+
+  if (is_online) {
+    resp <- httr::GET(url)
+  } else {
+    stop("Please check your internet connection.", call. = FALSE)
+  }
+
+  if (httr::http_error(resp)) {
+    stop(
+      sprintf(
+        "Request failed [%s]\n%s",
+        httr::status_code(resp),
+        result$error$message
+      ),
+      call. = FALSE
+    )
+  } else {
+    resp %>%
+      httr::content("text") %>%
+      jsonlite::fromJSON() %>%
+      magrittr::use_series(data)
+  }
+
+}
