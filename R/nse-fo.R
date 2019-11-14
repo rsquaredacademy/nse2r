c7f649484428c82e0e08acaf7299f73d14045a7b
rsquaredin <rsquaredin@gmail.com>
2019-11-14 13:35:19 +0530

    closes #37

--- R/nse-fo.R
+++ R/nse-fo.R
@@ -13,10 +13,28 @@ nse_fo_lotsizes <- function() {
 
   url <- "https://www.nseindia.com/content/fo/fo_mktlots.csv"
 
-  url %>%
+  result <-
+    url %>%
 		utils::read.csv() %>%
 		tibble::as_tibble()
-  
+
+  split_line <-
+    result %>%
+    magrittr::use_series(SYMBOL) %>%
+    as.character() %>%
+    trimws() %>%
+    magrittr::equals("Symbol") %>%
+    which()
+
+  index_line <- split_line - 1
+  stock_line <- split_line + 1
+
+  indices <- x[1:index_line,]
+  stocks  <- x[stock_line:nrow(x), ]
+
+  list(indices = indices,
+       stocks  = stocks)
+
 }
 
 #' NSE top F & O gainers
