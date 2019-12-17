865b87e115559ee8e5ce711a9451ce90c9075486
rsquaredin <rsquaredin@gmail.com>
2019-12-17 11:44:24 +0530

    closes #44

--- R/nse-utils.R
+++ R/nse-utils.R
@@ -10,24 +10,29 @@
 #'
 nse_base <- function(url) {
 
-  resp <- httr::GET(url)
+  # resp <- httr::GET(url)
+  #
+  # if (httr::http_error(resp)) {
+  #   stop(
+  #     sprintf(
+  #       "Request failed [%s]\n%s",
+  #       httr::status_code(resp),
+  #       result$error$message
+  #     ),
+  #     call. = FALSE
+  #   )
+  # } else {
+  #   resp %>%
+  #     httr::content("text", encoding = "UTF-8") %>%
+  #     jsonlite::fromJSON() %>%
+  #     magrittr::use_series(data) %>%
+  #     tibble::as_tibble()
+  # }
 
-  if (httr::http_error(resp)) {
-    stop(
-      sprintf(
-        "Request failed [%s]\n%s",
-        httr::status_code(resp),
-        result$error$message
-      ),
-      call. = FALSE
-    )
-  } else {
-    resp %>%
-      httr::content("text", encoding = "UTF-8") %>%
-      jsonlite::fromJSON() %>%
-      magrittr::use_series(data) %>%
-      tibble::as_tibble()
-  }
+  url %>%
+    jsonlite::fromJSON() %>%
+    magrittr::use_series(data) %>%
+    tibble::as_tibble()
 
 }
 
