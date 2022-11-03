6c87e6eb28bcd3272b156b53547fb86698a834d6
rsquaredin <rsquaredin@gmail.com>
2022-11-03 18:06:16 +0530

    test: add new tests from scratch

--- tests/testthat/test-advances-declines.R
+++ tests/testthat/test-advances-declines.R
@@ -1,13 +1,8 @@
-context("advances-declines")
-
 httptest::with_mock_api({
   test_that("output from nse_advances_declines is as expected", {
-    
+
     iquotes <- nse_advances_declines()
-    
+
     expect_equal(names(iquotes), c("index_name", "advances", "declines", "unchanged"))
   })
 })
-
-
-
